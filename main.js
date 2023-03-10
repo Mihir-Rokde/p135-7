de094432f54ba113753dedc70a76a20685329c9f
Mihir-Rokde <100846891+Mihir-Rokde@users.noreply.github.com>
2023-03-10 19:53:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 status="";
-
+objects=[];
 function setup(){
     canvas=createCanvas(380,380);
     canvas.center();
@@ -15,8 +15,39 @@ function start(){
 function modelloaded(){
     console.log("modelloaded");
     status=true;
-
+}
+function gotResult(error,results){
+if(error){
+    console.log(error);
+}
+console.log(results);
+objects=results;
 }
 function draw(){
-    image(video,0,0,380,380);
+image(video,0,0,380,380);
+if(status!=""){
+    objectDetector.detect(video,gotResult);
+    for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="status : objects detected";
+            fill("blue");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("blue");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+            if(objects[i].label==object_name){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("object_status").innerHTML=object_name+" found";
+                synth=window.speechSynthesis;
+                utterThis=new SpeechSynthesisUtterance(object_name+"found");
+                synth.speak(utterThis);
+            
+            }
+            else{
+                document.getElementById("object_status").innerHTML=object_name+" not found";
+            }
+        }
+}
 }
